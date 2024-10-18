e69a8c77c78ee3a33f06597db7fa4d48fe37d84d
NathaliaPadron <nbavutti@gmail.com>
2024-10-18 13:04:02 +0200

    categories controller

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -2,7 +2,7 @@ import Category from "../model/Category.js";
 import asyncHandler from "express-async-handler";
 
 // desc Create new category
-// @route POST /api/v1/category
+// @route POST /api/v1/category/create
 // @access Private/Admin
 
 export const createCategoryCtrl = asyncHandler(async(req,res)=>{
@@ -25,3 +25,62 @@ const categoryFound = await Category.findOne({ name });
     });
 })
 
+// desc GET all categories
+// @route GET /api/v1/category
+// @access Public
+
+export const getAllCategoriesCtrl = asyncHandler(async (req, res) => {
+    const categories = await Category.find();
+    res.json({
+      status: "success",
+      message: "Categories fetched successfully",
+      categories,
+    });
+  });
+  
+
+// desc GET Single category
+// @route GET /api/v1/category/:id
+// @access Public
+
+export const getCategoryCtrl = asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    
+    if(!category) {
+        throw new Error("Category not found!")
+    }
+    
+    res.json({
+      status: "success",
+      message: "Category fetched successfully",
+      category,
+    });
+
+  });
+  
+
+// desc Update Single category
+// @route PUT /api/v1/category/:id
+// @access Admin/Private
+
+export const updateCategoryCtrl = asyncHandler(async (req, res) => {
+
+    const { name } = req.body;
+
+    //update
+
+    const category = await Category.findByIdAndUpdate(req.params.id, {
+        name,
+    },
+    {
+        new:true,
+    });
+
+    res.json({
+        status: "success",
+        message: "category updated successfully",
+        category,
+      });
+
+  });
+  
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,9 +1,13 @@
  import express from "express";
-import { createCategoryCtrl } from "../controllers/categoryCtrl.js";
+import { createCategoryCtrl, getAllCategoriesCtrl, getCategoryCtrl, updateCategoryCtrl } from "../controllers/categoryCtrl.js";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
 
 const categoriesRouter = express.Router();
 
-categoriesRouter.post("/create", isLoggedIn, createCategoryCtrl)
+categoriesRouter.post("/create", isLoggedIn, createCategoryCtrl);
+categoriesRouter.get("/", getAllCategoriesCtrl);
+categoriesRouter.get("/:id", getCategoryCtrl);
+categoriesRouter.put("/:id", updateCategoryCtrl);
 
-export default categoriesRouter
+
+export default categoriesRouter;
